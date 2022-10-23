5a320ffda3b1c19665c12f0d6a4eb769769c747d
Matt Neal <neal.matt.m@gmail.com>
2022-10-23 05:44:28 -0400

    may the heroku gods have mercy on my soul

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
-
 const path = require('path');
 
 // import our typeDefs and resolvers
