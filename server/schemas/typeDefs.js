b76f1ccb44e3481e0db0b83942dbad880f294571
Matt Neal <neal.matt.m@gmail.com>
2022-10-22 20:26:10 -0400

    fleshed out graphql schema

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -12,6 +12,11 @@ const typeDefs = gql`
     friends: [User]
   }
 
+  type Auth {
+    token: ID!
+    user: User
+  }
+
   type Thought {
     _id: ID
     thoughtText: String
@@ -29,10 +34,19 @@ const typeDefs = gql`
   }
 
   type Query {
+    me: User
     users: [User]
     user(username: String!): User
     thoughts(username: String): [Thought]
     thought(_id: ID!): Thought
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
   }
 `;
 
